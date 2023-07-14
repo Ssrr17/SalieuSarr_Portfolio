1ed52ca472b5b69e472f08629e1bf0dcbf9fa77c
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2023-07-14 19:57:29 -0400

    experience component elelments added

--- src/components/experience/exp.jsx
+++ src/components/experience/exp.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import './exp.css'
+import React from "react";
+import "./exp.css";
+import { BsPatchCheck } from "react-icons/bs";
+
 const exp = () => {
   return (
-    <section id="">
-      Experience
-    </section>
-  )
-}
+    <section id="experience">
+      <h5>My Skills</h5>
+      <h2>Experience</h2>
 
-export default exp
+      <div className="container exp_container">
+        <div className="exp_front">
+          <h3> Frontend </h3>
+        
+        <div className="exp_content">
+          <article className="exp">
+            <BsPatchCheck />
+            <h4>HTML</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+          <article className="exp">
+            <BsPatchCheck />
+            <h4>CSS</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+          <article className="exp">
+            <BsPatchCheck />
+            <h4>JavaScript</h4>
+            <small className="text-light">Beginner</small>
+          </article>
+          <article className="exp">
+            <BsPatchCheck />
+            <h4>Bootstrap</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+          <article className="exp">
+            <BsPatchCheck />
+            <h4>React</h4>
+            <small className="text-light">Beginner</small>
+          </article>
+        </div>
+        </div>
+      
+      <div className="exp_back">
+        <h3> Backend </h3>
+      
+      <div className="exp_content">
+        <article className="exp">
+          <BsPatchCheck />
+          <h4>MySQL</h4>
+          <small className="text-light">Beginner</small>
+        </article>
+        <article className="exp">
+          <BsPatchCheck />
+          <h4>NodeJs</h4>
+          <small className="text-light">Beginner</small>
+        </article>
+        <article className="exp">
+          <BsPatchCheck />
+          <h4>NoSQL</h4>
+          <small className="text-light">Beginner</small>
+        </article>
+        <article className="exp">
+          <BsPatchCheck />
+          <h4>MongoDB</h4>
+          <small className="text-light">Beginner</small>
+        </article>
+      </div>
+      </div>
+      </div>
+
+    </section>
+  );
+};
+
+export default exp;
